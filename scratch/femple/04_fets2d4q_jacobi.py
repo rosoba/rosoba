5f282eb5db2776724d18f4c90c8dd1123d21d9a7
demian <demian@Laptop>
2012-05-22 14:25:59 +0200

    B_Matrix / Gauss Points

--- scratch/femple/04_fets2d4q_jacobi.py
+++ scratch/femple/04_fets2d4q_jacobi.py
@@ -4,10 +4,15 @@ Created on Apr 23, 2012
 @author: rch
 '''
 
-import numpy as np
-from enthought.traits.api import \
+import numpy as np 
+from numpy import \
+     array, zeros, int_, float_, ix_, dot, linspace, hstack, vstack, arange, \
+     identity
+from traits.api import \
     HasTraits, Array, Tuple, Constant, Property, cached_property, Float
 import sympy as sp
+from scipy.linalg import \
+     inv, det
 
 x_, y_, z_, t_ = sp.symbols('x,y,z,t')
 
@@ -57,22 +62,55 @@ class FETS2D4Q(HasTraits):
         '''Return kinematic matrix at the given points.
         '''
         B_mtx = np.zeros ((3, 4, 2), dtype = 'f')
+        
         dN_mtx = self.dN_fn(r, s)
+        
         B_mtx[self.B_idx_map] = dN_mtx[self.dN_idx_map]
+        
         return B_mtx.reshape(3, 8)
+
+    #===========================================================================
+    # Specification of global points
+    #===========================================================================
+    geo_x = Array(Float, value = [[0, 0], [3, 1], [2, 4], [-1, 2]])
+    #===========================================================================
+    # Generation of the B_Matrix
+    #===========================================================================
+    b_mtx = Property
+    
+    def _get_b_mtx(self):
+        gaus= np.array([[-5.777,-5.777],[5.777,-5.777],[5.777,5.777],[-5.777,5.777]], dtype ='f')
+        b_mtx = zeros ((2,4), dtype = 'f')
+
+        for i in range (0,3):
+            dN = self.dN_fn(gaus[i,0],gaus[i,1])
+            J_mtx = dot(dN,self.geo_x)
+            J_mtx_inv = inv(J_mtx)
+            b_mtx = b_mtx + dot(J_mtx_inv,dN)
+            
+        B = zeros ((3, 4, 2), dtype = 'f')
+
+
+        B[self.B_idx_map] = b_mtx[self.dN_idx_map]
+
+        return B.reshape(3, 8)
+    
+    
+    
+    #===========================================================================
+    # Kinematic mapping
+    #===========================================================================
+
 
 if __name__ == '__main__':
 
     fets = FETS2D4Q()
     
-    gauss_points = np.array(
-                            [[-0.5777, -0.5777],
-                             [0.5777, -0.5777],
-                             [0.5777, 0.5777],
-                             [-0.5777, 0.5777],
-                    ], dtype = 'f')
-    print 'gp', gauss_points[:, 0]
-    print 'dN_fn', fets.dN_fn(0., 0.)
-    print "B_mtx_map = \n", fets.get_B_mtx(gauss_points[:, 0], gauss_points[:, 1])
+   
+    
+    
+    
 
-    fets.configure_traits()
+print "B_mtx_map = \n", fets.b_mtx
+
+fets.configure_traits()
