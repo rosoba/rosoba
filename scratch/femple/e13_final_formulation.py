702e865f044899a4036df1cc9f3ca04a1bc0400c
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2012-10-09 11:10:29 +0200

    interface added

--- scratch/femple/e13_final_formulation.py
+++ scratch/femple/e13_final_formulation.py
@@ -8,7 +8,7 @@ import sys
 print sys.path
 
 from etsproxy.traits.api import \
-     Array, Bool, Callable, Enum, Float, HasTraits, Interface, implements, \
+     Array, Enum, Float, \
      Instance, Int, Trait, Str, Enum, Callable, List, TraitDict, Any, \
      on_trait_change, Tuple, WeakRef, Delegate, Property, cached_property, \
      Constant
